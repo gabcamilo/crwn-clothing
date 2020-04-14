274df30733187e01ff857c86401738a9723b5e0c
Gabriela Carvalho Camilo <gabccamilo.dev@gmail.com>
2020-04-13 21:03:46 -0300

    Added sign in page, not functional yet.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/HomePage';
 import ShopPage from './pages/shop/ShopPage';
+import SignInAndSignUp from './pages/sign-in-and-sign-up/SignInAndSignUp';
 import Header from './components/header/Header';
 import { Route, Switch } from "react-router-dom";
 
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage}/>
         <Route exact path="/shop" component={ShopPage}/>
+        <Route exact path="/signin" component={SignInAndSignUp}/>
       </Switch>
     </div>
   );
---
+++ src/components/sign-in/SignIn.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'
+import './SignIn.scss'
+import FormInput from '../form-input/FormInput'
+import CustomButton from '../custom-button/CustomButton'
+
+export class SignIn extends Component {
+	constructor(props){
+		super(props);
+
+		this.state={
+			email: '',
+			password: ''
+		}
+	}
+
+	handleSubmit = event => {
+		event.preventDefault();
+		this.setState({email:'', password:''})
+	}
+
+	handleChange = event => {
+		const {value, name} = event.target;
+		this.setState({[name]:value})
+	}
+
+	render() {
+		return (
+			<div className="sign-in">
+				<h2>Already have an account</h2>
+				<span>Sign in with your email and password</span>	
+
+				<form onSubmit={this.handleSubmit}>
+					<FormInput type="email" name="email" label="email" value={this.state.email} handleChange={this.handleChange} required/>
+					<FormInput type="password" name="password" label="password" value={this.state.password} handleChange={this.handleChange} required/>
+
+					<CustomButton type="submit">Sign In</CustomButton>
+				</form>
+			</div>
+		)
+	}
+}
+
+export default SignIn
