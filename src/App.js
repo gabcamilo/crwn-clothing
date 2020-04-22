1a2093c724335afde313c0339877c2bf5f00cc3d
Gabriela Carvalho Camilo <gabccamilo.dev@gmail.com>
2020-04-21 22:09:43 -0300

    Implemented store user data on state

--- src/App.js
+++ src/App.js
@@ -13,16 +13,29 @@ class App extends Component {
 
     this.state = {
       currentUser: null      
-    }
+    };
   }
 
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async user => {
-      // this.setState({ currentUser: user });
-      createUserProfileDocument(user)
-      // console.log(user);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if(userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          }, () =>{
+            console.log(this.state)
+          });
+        })
+      }
+
+      this.setState({ currentUser: userAuth})
     });
   }
 
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,6 +13,8 @@ const config = {
 	measurementId: "G-3QM06ZR2CQ"
 };
 
+firebase.initializeApp(config);
+
 export const createUserProfileDocument = async(userAuth, additionalData) => {
 	if(!userAuth) return;
 	const userRef = firestore.doc(`users/${userAuth.uid}`);
@@ -36,8 +38,6 @@ export const createUserProfileDocument = async(userAuth, additionalData) => {
 	return userRef;
 
 }
-
-firebase.initializeApp(config);
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
