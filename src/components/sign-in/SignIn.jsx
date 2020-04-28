c073c76a7bce54c774c0261b7374fbef54e58591
Gabriela Carvalho Camilo <gabccamilo.dev@gmail.com>
2020-04-27 22:44:13 -0300

    Finished SignIn component.

--- src/components/sign-in/SignIn.jsx
+++ src/components/sign-in/SignIn.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import './SignIn.scss'
 import FormInput from '../form-input/FormInput'
 import CustomButton from '../custom-button/CustomButton'
-import {signInWithGoogle} from '../../firebase/firebase.utils'
+import {signInWithGoogle, auth} from '../../firebase/firebase.utils'
 
 export class SignIn extends Component {
 	constructor(props){
@@ -14,8 +14,16 @@ export class SignIn extends Component {
 		}
 	}
 
-	handleSubmit = event => {
+	handleSubmit = async (event) => {
 		event.preventDefault();
+		const {email, password} = this.state;
+		try {
+			await auth.signInWithEmailAndPassword(email, password);
+			this.setState({email: '', password: ''});
+		}catch(error){
+			console.log(error);
+		}
+
 		this.setState({email:'', password:''})
 	}
 
