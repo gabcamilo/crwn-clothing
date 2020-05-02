767c4ce1bb3e00a7f0b0feadeb6c46315ae89847
Gabriela Carvalho Camilo <gabccamilo.dev@gmail.com>
2020-05-01 23:47:34 -0300

    CartIcon component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,6 +4,7 @@ import {ReactComponent as Logo} from '../../assets/icon.svg'
 import {Link} from 'react-router-dom'
 import {auth} from '../../firebase/firebase.utils'
 import {connect} from 'react-redux'
+import CartIcon from '../cart-icon/CartIcon'
 
 const Header = ({currentUser}) => {
 	return (
@@ -23,6 +24,7 @@ const Header = ({currentUser}) => {
 				) : (
 					<Link className='option' to='/signin'>SIGN IN</Link>
 				)}
+				<CartIcon />
 			</div>
 		</div>
 	)
