afc3c7d1a0b31e852ac7bc76106ade8fb2db58c4
Gabriela Carvalho Camilo <gabccamilo.dev@gmail.com>
2020-05-08 23:40:38 -0300

    Added reselect to implement memoised selector on the CartIcon component

--- src/components/cart-icon/CartIcon.jsx
+++ src/components/cart-icon/CartIcon.jsx
@@ -4,7 +4,7 @@ import {connect} from 'react-redux';
 import {toggleCartHidden} from '../../redux/cart/cart.actions'
 
 import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg'
-
+import {selectCartItemsCount} from '../../redux/cart/cart.selectors'
 
 import './CartIcon.scss'
 
@@ -21,8 +21,8 @@ const mapDispatchToProps = dispatch => ({
 	toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
-const mapStateToProps = ({cart: {cartItems}}) => ({
-	itemCount: cartItems.reduce((accumulator, cartItem) => accumulator += cartItem.quantity, 0)
+const mapStateToProps = (state) => ({
+	itemCount: selectCartItemsCount(state)
 })
 
 export default connect(
---
+++ src/redux/cart/cart.selectors.js
@@ -0,0 +1,23 @@
+import { createSelector} from 'reselect'
+
+//input selector
+const selectCart = state => state.cart;
+
+//output selector
+/**Takes two arguments
+ * 1- a collection (array) of input selectors
+ * 2- a function that will return the actual value we want.
+*/
+export const selectCartItems = createSelector(
+	[selectCart],
+	cart => cart.cartItems
+);
+
+export const selectCartItemsCount = createSelector(
+	[selectCartItems],
+	cartItems => cartItems.reduce(
+		(accumulatedQuantity, cartItem) =>
+		accumulatedQuantity + cartItem.quantity,
+		0
+	)
+)
